5028f2bbeebce1d9e30b583b5fbb1e54a8e95f5e
noarsoft <popthebreaker@hotmail.com>
2025-09-30 15:06:41 +0700

    update 2

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ const Express = require("express");
 const app = Express();
 const port = 3000;
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World!hjhgjhgjhgj");
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
